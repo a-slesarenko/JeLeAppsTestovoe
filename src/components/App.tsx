f565471ab18272eee65df83f1e0ca2b1e455aeb4
a-slesarenko <andreysveltov@gmail.com>
2025-03-07 12:47:47 +0300

    done

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as styles from "./App.module.scss";
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.square}></div>
+    </div>
+  );
+};
+
+export default App;
